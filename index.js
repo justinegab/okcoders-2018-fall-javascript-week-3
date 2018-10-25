60f23e15d186d064c7dbc6332770d6acadb38b55
Justine <jolyn1993@gmail.com>
2018-10-25 17:44:37 -0500

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ var _ = require('lodash');
 
 // Use package based on variable we import with
 
-for(var i = 0; i<5; i++){
-    var r = _.random(3,10);
-    console.log(r);
-}
+// for(var i = 0; i<5; i++){
+//     var r = _.random(3,10);
+//     console.log(r);
+// }
+
+// Create a command line to do list using Commander
+//  - Print menu
+//  - Add to List
+//  - Remove from List
+//  - Display List
+
+const args = process.argv;
+
+console.log(args)
+
+console.log('Choose an option:');
+console.log('Add to List:');
+console.log('Remove from List:');
+console.log('Print List:');
